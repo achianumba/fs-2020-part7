8c7c5358d0529c3ec643bc8f73409d28a7cc4f03
achianumba <chianumbaarinze@gmail.com>
2020-06-03 18:00:35 +0100

    Refactored single blog view to react-bootstrap component

--- bloglist/client/src/components/Blog.js
+++ bloglist/client/src/components/Blog.js
@@ -2,29 +2,32 @@ import React, { useState } from "react";
 import { useDispatch } from "react-redux";
 import { useHistory } from "react-router-dom";
 import { likeBlog, deleteBlog, addComment } from "../redux";
+import { Row, Col, Button, ListGroup, Form } from "react-bootstrap"; 
 
 const Blog = ({ blog }) => {
   const dispatch = useDispatch();
   const history = useHistory();
   const [newComment, setNewComment] = useState('');
 
+
   const blogOnChangeHandler = (e) => {
     e.preventDefault();
     let target = e.target;
 
     //Update number of likes
-    if (target.className === "like-blog") {
+    if (target.classList.contains("like-blog")) {
       return dispatch(likeBlog(blog.id, blog.likes + 1));
     }
 
     //Delete a blog
-    if (target.className === "delete-blog") {
+    if (target.classList.contains("delete-blog")) {
       dispatch(deleteBlog(blog.id));
       return history.push("/");
     }
 
     if (target.id === "add-comment") {
       dispatch(addComment(blog.id, newComment));
+      setNewComment('');
       return
     }
   };
@@ -34,33 +37,41 @@ const Blog = ({ blog }) => {
   if (!blog) return <></>;
 
   return (
-    <div className="blog" onClick={blogOnChangeHandler}>
-      <h3 className="blog-title" data-id={blog.id}>
+    <Row className="px-4" onClick={blogOnChangeHandler}>
+      <Col sm={12}>
+      <h2 className="text-center display-4">
         {blog.title}
-      </h3>
-      <p className="blog-author">{blog.author}</p>
-      <p className="blog-likes">
+      </h2>
+      <p className="blog__content mb-5">{blog.author}</p>
+      <p className="blog__likes mb-4">
         <strong>Likes: </strong>
         {blog.likes}{" "}
-        <button className="like-blog" data-id={blog.id}>
+        <Button className="like-blog ml-4">
           Like
-        </button>
+        </Button>
       </p>
-      <p className="blog-url">{blog.url}</p>
-      <button className="delete-blog" data-id={blog.id}>
+      <p className="blog__url">{blog.url}</p>
+      <Button className="mb-5">
         Delete
-      </button>
+      </Button>
 
       <h3>Comments</h3>
-      <ul>
+      <ListGroup variant="flush">
         {blog.comments.map((comment, index) => (
-          <li key={index}>{comment}</li>
+          <ListGroup.Item key={index} action className="comment">
+            {comment}<br />
+            <strong className="text-dark">Anonymous</strong>
+          </ListGroup.Item>
         ))}
-      </ul>
+      </ListGroup>
 
-      <textarea id="new-comment" value={ newComment } onChange={ e => setNewComment(e.target.value)}></textarea>
-      <button id="add-comment">Add comment</button>
-    </div>
+      <Form.Group controlId="new-comment" className="mt-4">
+        <Form.Label><strong>Join the conversation</strong></Form.Label>
+        <Form.Control as="textarea" rows="5" value={ newComment } onChange={ e => setNewComment(e.target.value)}></Form.Control>
+      </Form.Group>
+      <Button id="add-comment">Add comment</Button>
+      </Col>
+    </Row>
   );
 };
 
